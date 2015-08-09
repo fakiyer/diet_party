bca8ff3492cbe8aa81f35e47f957e06e87ff6036
fakiyer <xmnwt558@yahoo.co.jp>
2015-08-09 21:07:05 +0900

    Facebook認証実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'welcome#index'
-  
+
   get '*anyting' => 'errors#routing_error'
 end
